dab867d2ae7f3079077d827162a74daf7306198d
Ken Schnall <kenschnall@pm.me>
2022-12-18 13:39:01 -0600

    Rename ps to pst and add pdk

--- add-aliases.sh
+++ add-aliases.sh
@@ -10,9 +10,10 @@ echo '' >> ~/.bashrc
 
 echo '# kubuntu-scripts' >> ~/.bashrc
 echo 'alias pd="protonvpn-cli ns --off && protonvpn-cli ks --off && protonvpn-cli d && protonvpn-cli s"' >> ~/.bashrc
+echo 'alias pdk="protonvpn-cli ks --permanent && protonvpn-cli d && protonvpn-cli s"' >> ~/.bashrc
 echo 'alias pf="protonvpn-cli ns --ads-malware && protonvpn-cli ks --permanent && protonvpn-cli c -f && protonvpn-cli s"' >> ~/.bashrc
 echo 'alias pr="protonvpn-cli ns --ads-malware && protonvpn-cli ks --permanent && protonvpn-cli c --cc RU && protonvpn-cli s"' >> ~/.bashrc
-echo 'alias ps="protonvpn-cli s"' >> ~/.bashrc
+echo 'alias pst="protonvpn-cli s"' >> ~/.bashrc
 echo 'alias t="~/repos/kubuntu-scripts/time-theme/time-theme.py"' >> ~/.bashrc
 echo 'alias td="~/repos/kubuntu-scripts/time-theme/time-theme.py --dark"' >> ~/.bashrc
 echo 'alias tl="~/repos/kubuntu-scripts/time-theme/time-theme.py --light"' >> ~/.bashrc
