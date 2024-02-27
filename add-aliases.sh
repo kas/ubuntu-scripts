bd107e7acc47ad954c997711ab1fc50c9ed3e05d
Ken Schnall <kenschnall@pm.me>
2024-02-26 21:52:16 -0600

    Update add-aliases.sh

--- add-aliases.sh
+++ add-aliases.sh
@@ -8,7 +8,7 @@ if [ ${bashrc: -1} != '$' ]; then
 fi
 echo '' >> ~/.bashrc
 
-echo '# kubuntu-scripts' >> ~/.bashrc
+echo '# ubuntu-scripts' >> ~/.bashrc
 echo 'alias ab="sudo auto-cpufreq --force=reset"' >> ~/.bashrc
 echo 'alias ap="sudo auto-cpufreq --force=powersave"' >> ~/.bashrc
 echo 'alias as="auto-cpufreq --stats"' >> ~/.bashrc
@@ -16,6 +16,7 @@ echo 'alias pd="protonvpn-cli ks --off && protonvpn-cli d && protonvpn-cli s"' >
 echo 'alias pdk="protonvpn-cli ks --permanent && protonvpn-cli d && protonvpn-cli s"' >> ~/.bashrc
 echo 'alias pf="protonvpn-cli ns --ads-malware && protonvpn-cli ks --permanent && protonvpn-cli c -f && protonvpn-cli s"' >> ~/.bashrc
 echo 'alias pst="protonvpn-cli s"' >> ~/.bashrc
+echo 'alias psw="protonvpn-cli ns --ads-malware && protonvpn-cli ks --permanent && protonvpn-cli c --cc CH && protonvpn-cli s"' >> ~/.bashrc
 echo 'alias t="~/repos/kubuntu-scripts/time-theme/time-theme.py"' >> ~/.bashrc
 echo 'alias td="~/repos/kubuntu-scripts/time-theme/time-theme.py --day"' >> ~/.bashrc
 echo 'alias tn="~/repos/kubuntu-scripts/time-theme/time-theme.py --night"' >> ~/.bashrc
